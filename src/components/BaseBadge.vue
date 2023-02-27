3012caa828121025311411e3b6b08d606d1d2fa4
arman-borkhani <armanborkhani@gmail.com>
2023-02-27 21:45:15 +0330

    Add badge component

---
+++ src/components/BaseBadge.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="badge">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.badge {
+  background-color: #8d006e;
+  color: #fff;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  font-size: 0.75rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import store from './store/index.js';
 import router from './router.js';
 import App from './App.vue';
 import BaseCard from './components/BaseCard.vue';
+import BaseBadge from './components/BaseBadge.vue';
 
 const app = createApp(App);
 
@@ -10,4 +11,6 @@ app.use(router);
 app.use(store);
 
 app.component('base-card', BaseCard);
+app.component('base-badge', BaseBadge);
+
 app.mount('#app');
