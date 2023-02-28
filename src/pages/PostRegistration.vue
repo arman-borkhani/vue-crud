3909af9c77a1ec8d45c2fef88e7ac7defd869025
arman-borkhani <armanborkhani@gmail.com>
2023-02-28 19:32:28 +0330

    Add post registration

--- src/pages/PostRegistration.vue
+++ src/pages/PostRegistration.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Add Post</h1>
+  <div class="container">
+    <h1>Add New Post</h1>
+    <post-form @submit-form="savePost"></post-form>
+  </div>
 </template>
+
+<script>
+import PostForm from '../components/PostForm.vue';
+
+export default { 
+  components: {
+    PostForm
+  },
+  methods: {
+    savePost(data) {
+      const formData = {
+        title: data.title,
+        desc: data.desc,
+        cat: data.cat
+      };
+
+      this.$store.dispatch('registerPost', formData);
+      this.$router.replace('/posts');
+    },
+  }
+}
+</script>
