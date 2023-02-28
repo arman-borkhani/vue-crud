265e824e8e425bf02d90457fb15846ce49010129
arman-borkhani <armanborkhani@gmail.com>
2023-02-28 19:35:25 +0330

    Add 404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>404</h1>
+  <div class="container">
+    <h1>Page Not Found</h1>
+    <base-card>
+      <p>The link you followed is probably broken, or the page has beed removed.</p>
+    </base-card>
+  </div>
 </template>
